7de5b992a3fdb7f657faec20642b9dd4a3361563
CrescentLua <45102273+CrescentLua@users.noreply.github.com>
2019-09-21 16:51:11 -0400

    EZ

--- Crescent2D/Assets/Character.cs
+++ Crescent2D/Assets/Character.cs
@@ -2,28 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Character : MonoBehaviour
 {
     private Rigidbody2D rb;
-    private Vector2 movePlayer; 
-    public float WalkSpeed; 
+    private Vector2 movePlayerHorizontal;
+    private Vector2 movePlayerVertical;
+    public float WalkSpeed;
+    public float JumpHeight; 
     
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-        WalkSpeed = 10.0f; 
+        WalkSpeed = 10.0f;
+        JumpHeight = 10.0f; 
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 moveVector = new Vector2(Input.GetAxis("Horizontal"), 0.0f);
-        movePlayer = moveVector * WalkSpeed;
+        Vector3 horizontal = new Vector3(Input.GetAxis("Horizontal"), 0.0f);
+        transform.position += horizontal * WalkSpeed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("Is Jumping lol");
+            rb.AddForce(new Vector2(0.0f, JumpHeight), ForceMode2D.Impulse);
+        }
+     
     }
 
-    void FixedUpdate()
-    {
-        rb.MovePosition(rb.position + movePlayer * Time.fixedDeltaTime);
-    }
 }
