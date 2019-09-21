dc5b0e961bef454a6c35b452e95701102bc4aa56
CrescentLua <45102273+CrescentLua@users.noreply.github.com>
2019-09-21 16:16:53 -0400

    Big Boi

---
+++ Crescent2D/Assets/Character.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Vector2 movePlayer; 
+    public float WalkSpeed; 
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        WalkSpeed = 10.0f; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 moveVector = new Vector2(Input.GetAxis("Horizontal"), 0.0f);
+        movePlayer = moveVector * WalkSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movePlayer * Time.fixedDeltaTime);
+    }
+}
--- Crescent2D/Assets/Scripts/CameraScript.cs
+++ Crescent2D/Assets/Scripts/CameraScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CameraFollow : MonoBehaviour
+public class CameraScript : MonoBehaviour
 {
 
     public GameObject Player;
