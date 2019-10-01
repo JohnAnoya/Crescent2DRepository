1794e8088637df5df0a82a58b56069737a58f4fa
CrescentLua <45102273+CrescentLua@users.noreply.github.com>
2019-10-01 10:33:49 -0400

    Fixed Crescent Following Script

--- Crescent2D/Assets/Scripts/CrescentFollow.cs
+++ Crescent2D/Assets/Scripts/CrescentFollow.cs
@@ -6,10 +6,13 @@ public class CrescentFollow : MonoBehaviour
 {
 
     public GameObject Player;
+    float NewPosition;
+    const float Subtractor = 1.5f;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        NewPosition = this.transform.position.y + 3.0f;
     }
 
     // Update is called once per frame
@@ -17,29 +20,7 @@ public class CrescentFollow : MonoBehaviour
     {
         if (Player)
         {
-            this.transform.position = new Vector3(Player.transform.position.x - 2.0f, Player.transform.position.y + 1.0f, this.transform.position.z);
-            StartCoroutine(CrescentBobbing());
+            this.transform.position = new Vector3(Player.transform.position.x - 2.0f, Mathf.PingPong(Time.time, NewPosition) - Subtractor * NewPosition, this.transform.position.z);
         }
-    }
-
-    IEnumerator CrescentBobbing()
-    {
-        yield return new WaitForSeconds(4.0f);
-        
-        for (int i = 0; i < 5; i++)
-        {
-            this.transform.position = new Vector3(Player.transform.position.x, this.transform.position.y - i, this.transform.position.z);
-
-            yield return new WaitForSeconds(1.3f);
-
-            for (int j = 0; j < 5; j++)
-            {
-                this.transform.position = new Vector3(Player.transform.position.x, this.transform.position.y + j, this.transform.position.z);
-            }
-        }
-
-
-    
-
     }
 }
