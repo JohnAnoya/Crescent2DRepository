e87020cdbe0cf57974b530a4a84b3f96838fd770
CrescentLua <45102273+CrescentLua@users.noreply.github.com>
2019-11-01 15:57:17 -0400

    Added Level UI viewing

--- Crescent2D/Assets/Scripts/CharacterBoardMovement.cs
+++ Crescent2D/Assets/Scripts/CharacterBoardMovement.cs
@@ -2,15 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
 
 public class CharacterBoardMovement : MonoBehaviour
 {
 
-    float WalkSpeed; 
+    TextMeshProUGUI Title;
+    TextMeshProUGUI Level;
+
+    public Transform UIPopUp; 
+
+    float WalkSpeed;
+
+    bool UIOpen;
+
+    void Awake()
+    {
+        
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        WalkSpeed = 12.0f; 
+        WalkSpeed = 12.0f;
+        UIOpen = false; 
     }
 
     // Update is called once per frame
@@ -22,15 +38,37 @@ public class CharacterBoardMovement : MonoBehaviour
 
     void OnTriggerStay2D(Collider2D collision)
     {
-       if(collision.tag == "Level" && Input.GetButtonDown("Submit"))
+        if (UIOpen == false)
+        {
+            UIOpen = true; 
+
+            var SpawnedUI = Instantiate(UIPopUp, new Vector3(gameObject.transform.position.x + 7.0f, gameObject.transform.position.y + 7.0f, UIPopUp.transform.position.z), gameObject.transform.rotation);
+            SpawnedUI.parent = GameObject.Find("UI").transform;
+            SpawnedUI.name = "MapUIPopUp";
+
+            Title = GameObject.Find("UI/MapUIPopUp/Panel/Title").GetComponent<TextMeshProUGUI>();
+            Level = GameObject.Find("UI/MapUIPopUp/Panel/Level").GetComponent<TextMeshProUGUI>();
+
+            Level.text = "Level: " + collision.gameObject.name;
+            Title.text = collision.gameObject.transform.Find("Title").GetComponent<Text>().text;
+        }
+
+
+        if (collision.tag == "Level" && Input.GetButtonDown("Submit"))
         {
             Debug.Log("Player hit Enter");
-            SceneManager.LoadScene(collision.gameObject.name);
+            SceneManager.LoadScene("Level" + collision.gameObject.name);
         }
 
        else if (collision.tag == "Tutorial" && Input.GetButtonDown("Submit"))
         {
            SceneManager.LoadScene("Tutorial");
         }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        Destroy(GameObject.Find("MapUIPopUp"));
+        UIOpen = false; 
     }
 }
