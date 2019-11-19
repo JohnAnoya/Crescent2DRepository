cc6eccf64f028e713850f63667f241c4710465ae
AlexanderDispenza <alexanderdispenza@gmail.com>
2019-11-18 23:19:16 -0500

    Added Fan platforming section

---
+++ Crescent2D/Assets/Scripts/FanSoundTrigger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FanSoundTrigger : MonoBehaviour
+{
+	public AudioClip fanSnd;
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (collision.gameObject.CompareTag("Player"))
+		{
+			AudioManager.instance.alterPitchEffect(fanSnd);
+		}
+	}
+}
